b65ceef9e372715b991989c80638e48187c3ef0a
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-14 00:44:49 +0100

    First commit

---
+++ src/main/java/net/higherAchevers/entity/Greeting.java
@@ -0,0 +1,12 @@
+package net.higherAchevers.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Greeting {
+    private String msg;
+    private String from;
+    private String to;
+}
